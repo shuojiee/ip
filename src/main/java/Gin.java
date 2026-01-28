11e5cea5d0afd647086787b97550c2865d5f3af4
Aw Shuo Jie <178105654+shuojiee@users.noreply.github.com>
2026-01-28 12:52:17 +0800

    Rename chatbot to Gin and implement skeleton

---
+++ src/main/java/Gin.java
@@ -0,0 +1,13 @@
+public class Gin {
+    private static final String HORIZONTAL_LINE = "_".repeat(60);
+
+    public static void main(String[] args) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello! I'm Gin.\nWhat can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
+
+
