a790cf70e57f3119ee917b2ff0f39dd376bd6281
Aw Shuo Jie <178105654+shuojiee@users.noreply.github.com>
2026-01-30 03:52:15 +0800

    Implemented list to store and return user input

--- src/main/java/Gin.java
+++ src/main/java/Gin.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Gin {
     private static final String HORIZONTAL_LINE = "_".repeat(60);
+    private static String[] list = new String[100];
+    private static int stringCount = 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,14 +17,34 @@ public class Gin {
 
             if (userInput.equals("bye")) {
                 System.out.println(HORIZONTAL_LINE);
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("    Bye. Hope to see you again soon!");
                 System.out.println(HORIZONTAL_LINE);
                 break;
             }
 
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println("    " + userInput);
-            System.out.println(HORIZONTAL_LINE);
+            if (userInput.equals("list")) {
+                if (stringCount <= 0) {
+                    System.out.println(HORIZONTAL_LINE);
+                    System.out.println("    list is empty");
+                    System.out.println(HORIZONTAL_LINE);
+                } else {
+                    System.out.println(HORIZONTAL_LINE);
+
+                    for (int i = 1; i <= stringCount; i++) {
+                        System.out.println("    " + i + ". " + list[i - 1]);
+                    }
+
+                    System.out.println(HORIZONTAL_LINE);
+                }
+            } else {
+                list[stringCount] = userInput;
+
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("    added: " + userInput);
+                System.out.println(HORIZONTAL_LINE);
+
+                stringCount++;
+            }
         }
 
         scanner.close();
